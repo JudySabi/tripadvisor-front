17216631b720760ea380ca06ffa8755165349dd6
Julie Mahe <j-mahe@hotmail.fr>
2021-02-02 10:58:15 +0100

    warning

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Document chargé");
+
+  //   const contactForm = document.querySelector("#contactForm");
+  //   contactForm.addEventListener("submit", async (event) => {
+  //     event.preventDefault();
+  //     const data = {
+  //       firstname: document.querySelector("#firstname").value,
+  //       lastname: document.querySelector("#lastname").value,
+  //       email: document.querySelector("#email").value,
+  //       subject: document.querySelector("#subject").value,
+  //       message: document.querySelector("#message").value,
+  //     };
+  //     console.log(data);
+  //     const response = await axios.post(
+  //       " https://mahe-form-back.herokuapp.com/",
+  //       data
+  //     );
+  //     console.log(response);
+
+  //     if (response.status !== 200) {
+  //       alert("Le formulaire n'a pas pu être envoyé !");
+  //     } else {
+  //       alert("Le formulaire à bien été envoyé !");
+  //     }
+  //   });
+});
